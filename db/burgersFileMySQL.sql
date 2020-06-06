a5fdd9c005c3a1633216e6709c24eb807eaa5611
Jon Christie <jonpchristie@gmail.com>
2020-06-06 10:15:07 -0400

    merged

--- db/burgersFileMySQL.sql
+++ db/burgersFileMySQL.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INT auto_increment PRIMARY KEY,
+    id INT auto increment PRIMARY KEY,
     burger_name VARCHAR(255),
     devoured BOOLEAN,
 );
